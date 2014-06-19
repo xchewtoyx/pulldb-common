56583dd5f632e085efb9e3524a47a5b28cc2f99b
Russell Heilling <russell@linx.net>
2014-06-19 21:59:33 +0100

    Move subscription_key method into the model to match other classes

--- pulldb/models/subscriptions.py
+++ pulldb/models/subscriptions.py
@@ -3,15 +3,20 @@ from datetime import datetime
 
 from google.appengine.ext import ndb
 
+# pylint: disable=F0401
+
+from pulldb.models import users
 from pulldb.models import volumes
 
-class Subscription(ndb.Model):
-  '''Subscription object in datastore.
+# pylint: disable=W0232,E1101,R0903,R0201,C0103,W0201
 
-  Holds subscription data. Parent should be User.
-  '''
-  start_date = ndb.DateProperty(default=datetime.min)
-  volume = ndb.KeyProperty(kind=volumes.Volume)
+class Subscription(ndb.Model):
+    '''Subscription object in datastore.
+
+    Holds subscription data. Parent should be User.
+    '''
+    start_date = ndb.DateProperty(default=datetime.min)
+    volume = ndb.KeyProperty(kind=volumes.Volume)
 
 @ndb.tasklet
 def subscription_context(subscription):
@@ -22,3 +27,20 @@ def subscription_context(subscription):
         'volume': volume,
         'publisher': publisher,
     })
+
+def subscription_key(volume_key, create=False):
+    user = users.user_key()
+    volume_key = ndb.Key(urlsafe=volume_key)
+    key = ndb.Key(
+        Subscription, volume_key.id(),
+        parent=user,
+    )
+    subscription = key.get()
+    if not subscription and create:
+        subscription = Subscription(
+            key=key,
+            volume=volume_key
+        )
+        key = subscription.put()
+    if subscription:
+        return key
